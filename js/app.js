c5965b0214a3e1f171ce271169e9ed9db6da3339
meddlingteens <BRAD@WOLVESATBAY.NET>
2026-02-19 18:19:09 -0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -3,55 +3,49 @@
 // Subhead + Banner Rotation
 // =============================
 
-document.addEventListener("DOMContentLoaded", () => {
+/* =============================
+   Random Subhead Messages
+============================= */
 
-  /* =============================
-     Random Subhead Messages
-  ============================= */
+const subheadMessages = [
+  "Just, eeuuuuu.",
+  "Ain't no one wanna see that.",
+  "Hide your shame.",
+  "Seriously, that's gross.",
+  "I can't unsee that.",
+  "WTF?",
+  "Place a pixel where the good lord split yea.",
+  "Leave everything for your imagination.",
+  "Uh, really?",
+  "Yeah, nah, yeah, nah, nah, nah.",
+  "I think I just puked a little in my mouth.",
+  "Don't be fickle, apply a pixel."
+];
 
-  const subheadMessages = [
-    "Just, eeuuuuu.",
-    "Ain't no one wanna see that.",
-    "Hide your shame.",
-    "Seriously, that's gross.",
-    "I can't unsee that.",
-    "WTF?",
-    "Place a pixel where the good lord split yea.",
-    "Leave everything for your imagination.",
-    "Uh, really?",
-    "Yeah, nah, yeah, nah, nah, nah.",
-    "I think I just puked a little in my mouth.",
-    "Don't be fickle, apply a pixel."
-  ];
+const subheadEl = document.getElementById("subhead");
 
-  const subheadEl = document.getElementById("subhead");
-
-  if (subheadEl) {
-    const randomSubhead =
-      subheadMessages[Math.floor(Math.random() * subheadMessages.length)];
-    subheadEl.textContent = randomSubhead;
-  }
+if (subheadEl) {
+  subheadEl.textContent =
+    subheadMessages[Math.floor(Math.random() * subheadMessages.length)];
+}
 
 
-  /* =============================
-     Random Banner Headlines
-  ============================= */
+/* =============================
+   Random Banner Headlines
+============================= */
 
-  const bannerMessages = [
-    "Buy something you really don't need",
-    "Shop mofo. Buy, buy, buy",
-    "This is where you can advertise your useless crap",
-    "What the world really needs is more advertising",
-    "Wanna buy one of those endlessly spinning top things?",
-    "Sell stuff here, bitches"
-  ];
+const bannerMessages = [
+  "Buy something you really don't need",
+  "Shop mofo. Buy, buy, buy",
+  "This is where you can advertise your useless crap",
+  "What the world really needs is more advertising",
+  "Wanna buy one of those endlessly spinning top things?",
+  "Sell stuff here, bitches"
+];
 
-  const bannerHeading = document.getElementById("bannerHeading");
+const bannerHeading = document.getElementById("bannerHeading");
 
-  if (bannerHeading) {
-    const randomBanner =
-      bannerMessages[Math.floor(Math.random() * bannerMessages.length)];
-    bannerHeading.textContent = randomBanner;
-  }
-
-});
+if (bannerHeading) {
+  bannerHeading.textContent =
+    bannerMessages[Math.floor(Math.random() * bannerMessages.length)];
+}
