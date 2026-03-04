86457d9a896ab2a827fe9f1c402cb0b11edfc5d3
meddlingteens <BRAD@WOLVESATBAY.NET>
2026-03-04 09:08:44 -0600

    Add files via upload

---
+++ js/service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "pixelated-app-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/js/app.js",
+  "/js/pixelWorker.js",
+  "/manifest.json",
+  "/images/backgroundPixelsLarge.webp"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
